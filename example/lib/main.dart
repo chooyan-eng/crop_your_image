7cf646ca9d734511891c8619161c05741789fb50
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2021-03-19 09:19:30 +0900

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:typed_data';
 
 import 'package:crop_your_image/crop_your_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -15,12 +17,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('crop_your_image Sample'),
+          title: Text('Crop Your Image Demo'),
         ),
         body: CropSample(),
       ),
@@ -34,31 +37,51 @@ class CropSample extends StatefulWidget {
 }
 
 class _CropSampleState extends State<CropSample> {
-  final _cropController = CropController();
-
-  Uint8List? _image;
-  Uint8List? _croppedImage;
-
-  bool _isCropping = false;
-  bool _isCircle = false;
-  double? _selectedAspectRatio;
-
-  static const _aspectRatioSelection = <double?>[
-    9 / 16,
-    3 / 4,
-    1 / 1,
-    4 / 3,
-    16 / 9,
-    null
+  static const _images = const [
+    'assets/images/city.png',
+    'assets/images/lake.png',
+    'assets/images/train.png',
+    'assets/images/turtois.png',
   ];
+
+  final _cropController = CropController();
+  final _imageDataList = <Uint8List>[];
+
+  var _loadingImage = false;
+  var _currentImage = 0;
+  set currentImage(int value) {
+    setState(() {
+      _currentImage = value;
+    });
+    _cropController.image = _imageDataList[_currentImage];
+  }
+
+  var _isSumbnail = false;
+  var _isCropping = false;
+  var _isCircleUi = false;
+  Uint8List? _croppedData;
 
   @override
   void initState() {
-    rootBundle.load('assets/images/big_pic.png').then((assetData) {
-      final image = assetData.buffer.asUint8List();
-      setState(() => _image = image);
-    });
+    _loadAllImages();
     super.initState();
+  }
+
+  Future<void> _loadAllImages() async {
+    setState(() {
+      _loadingImage = true;
+    });
+    for (final assetName in _images) {
+      _imageDataList.add(await _load(assetName));
+    }
+    setState(() {
+      _loadingImage = false;
+    });
+  }
+
+  Future<Uint8List> _load(String assetName) async {
+    final assetData = await rootBundle.load(assetName);
+    return assetData.buffer.asUint8List();
   }
 
   @override
@@ -68,90 +91,168 @@ class _CropSampleState extends State<CropSample> {
       height: double.infinity,
       child: Center(
         child: Visibility(
-          visible: _croppedImage == null,
-          child: _isCropping
-              ? CircularProgressIndicator()
-              : Column(
-                  children: [
-                    Expanded(
-                      child: _image != null
-                          ? Crop(
-                              image: _image!,
-                              onCropped: (croppedImage) {
-                                setState(() {
-                                  _croppedImage = croppedImage;
-                                  _isCropping = false;
-                                });
-                              },
-                              withCircleUi: _isCircle,
-                              aspectRatio: _selectedAspectRatio,
-                              initialSize: 0.5,
-                              controller: _cropController,
-                            )
-                          : const SizedBox(),
-                    ),
-                    const SizedBox(height: 32),
-                    ToggleButtons(
-                      isSelected: _aspectRatioSelection
-                          .map((value) => value == _selectedAspectRatio)
-                          .toList(),
-                      onPressed: (index) {
-                        _cropController.aspectRatio =
-                            _aspectRatioSelection[index];
-                        setState(() {
-                          _selectedAspectRatio = _aspectRatioSelection[index];
-                        });
-                      },
-                      children: _aspectRatioSelection
-                          .map<Widget>((value) => Text(value == null
-                              ? 'null'
-                              : '${(value * 10).toInt() / 10}'))
-                          .toList(),
-                    ),
-                    const SizedBox(height: 16),
-                    ElevatedButton(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: Text('Change shape'),
-                      ),
-                      onPressed: () {
-                        _isCircle = !_isCircle;
-                        _cropController.withCircleUi = _isCircle;
-                      },
-                    ),
-                    const SizedBox(height: 60),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16.0),
-                            child: Text('Crop square!'),
-                          ),
-                          onPressed: () {
-                            setState(() => _isCropping = true);
-                            _cropController.crop();
-                          },
-                        ),
-                        const SizedBox(width: 32),
-                        ElevatedButton(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16.0),
-                            child: Text('Crop circle!'),
-                          ),
-                          onPressed: () {
-                            setState(() => _isCropping = true);
-                            _cropController.cropCircle();
-                          },
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 120),
-                  ],
+          visible: !_loadingImage && !_isCropping,
+          child: Column(
+            children: [
+              if (_imageDataList.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      _buildSumbnail(_imageDataList[0]),
+                      const SizedBox(width: 16),
+                      _buildSumbnail(_imageDataList[1]),
+                      const SizedBox(width: 16),
+                      _buildSumbnail(_imageDataList[2]),
+                      const SizedBox(width: 16),
+                      _buildSumbnail(_imageDataList[3]),
+                    ],
+                  ),
                 ),
-          replacement: _croppedImage == null
-              ? SizedBox.shrink()
-              : Image.memory(_croppedImage!),
+              Expanded(
+                child: Visibility(
+                  visible: _croppedData == null,
+                  child: Stack(
+                    children: [
+                      if (_imageDataList.isNotEmpty)
+                        Crop(
+                          controller: _cropController,
+                          image: _imageDataList[_currentImage],
+                          onCropped: (croppedData) {
+                            setState(() {
+                              _croppedData = croppedData;
+                              _isCropping = false;
+                            });
+                          },
+                          withCircleUi: _isCircleUi,
+                          initialSize: 0.5,
+                          maskColor: _isSumbnail ? Colors.white : null,
+                          cornerDotBuilder: (size, index) => _isSumbnail
+                              ? const SizedBox.shrink()
+                              : const DotControl(),
+                        ),
+                      Positioned(
+                        right: 16,
+                        bottom: 16,
+                        child: GestureDetector(
+                          onTapDown: (_) => setState(() => _isSumbnail = true),
+                          onTapUp: (_) => setState(() => _isSumbnail = false),
+                          child: CircleAvatar(
+                            backgroundColor:
+                                _isSumbnail ? Colors.blue.shade50 : Colors.blue,
+                            child: Center(
+                              child: Icon(Icons.crop_free_rounded),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  replacement: Center(
+                    child: _croppedData == null
+                        ? SizedBox.shrink()
+                        : Image.memory(_croppedData!),
+                  ),
+                ),
+              ),
+              if (_croppedData == null)
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.crop_7_5),
+                            onPressed: () {
+                              _isCircleUi = false;
+                              _cropController.aspectRatio = 16 / 4;
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.crop_16_9),
+                            onPressed: () {
+                              _isCircleUi = false;
+                              _cropController.aspectRatio = 16 / 9;
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.crop_5_4),
+                            onPressed: () {
+                              _isCircleUi = false;
+                              _cropController.aspectRatio = 4 / 3;
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.crop_square),
+                            onPressed: () {
+                              _isCircleUi = false;
+                              _cropController
+                                ..withCircleUi = false
+                                ..aspectRatio = 1;
+                            },
+                          ),
+                          IconButton(
+                              icon: Icon(Icons.circle),
+                              onPressed: () {
+                                _isCircleUi = true;
+                                _cropController.withCircleUi = true;
+                              }),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _isCropping = true;
+                            });
+                            _isCircleUi
+                                ? _cropController.cropCircle()
+                                : _cropController.crop();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            child: Text('CROP IT!'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+          replacement: const CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildSumbnail(Uint8List data) {
+    final index = _imageDataList.indexOf(data);
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          _croppedData = null;
+          currentImage = index;
+        },
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+            border: index == _currentImage
+                ? Border.all(
+                    width: 8,
+                    color: Colors.blue,
+                  )
+                : null,
+          ),
+          child: Image.memory(
+            data,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
