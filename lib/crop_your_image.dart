cc0c58eb89fb8da898936f6b74fad3a034c7eab5
airy <thanks.xa@gmail.com>
2021-07-14 23:36:30 +0900

    feat: add edge alignment

--- lib/crop_your_image.dart
+++ lib/crop_your_image.dart
@@ -10,3 +10,4 @@ import 'package:image/image.dart' as image;
 part 'src/crop.dart';
 part 'src/controller.dart';
 part 'src/calculator.dart';
+part 'src/edge_alignment.dart';
---
+++ lib/src/edge_alignment.dart
@@ -0,0 +1,9 @@
+part of crop_your_image;
+
+/// position of dot control
+enum EdgeAlignment {
+  topLeft,
+  topRight,
+  bottomLeft,
+  bottomRight,
+}
