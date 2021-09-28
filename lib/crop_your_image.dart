c9776f53f273afe203f6266edab6907e35a625bc
Glenn Ruysschaert <glenn@getdriven.app>
2021-09-28 10:56:10 +0200

    0.6.1

--- lib/crop_your_image.dart
+++ lib/crop_your_image.dart
@@ -3,11 +3,16 @@ library crop_your_image;
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:crop_your_image/src/detectors/custom_pan_detector.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as image;
 
-part 'src/crop.dart';
-part 'src/controller.dart';
 part 'src/calculator.dart';
+
+part 'src/controller.dart';
+
+part 'src/crop.dart';
+
 part 'src/edge_alignment.dart';
