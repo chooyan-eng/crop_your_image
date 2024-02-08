cdb3e8f2e30d128df77570c1b3d1ffdb510bdd0a
chooyan-eng <chooyan.engineer@gmail.com>
2024-02-08 17:52:31 +0900

    fix exports

---
+++ lib/src/logic/logic.dart
@@ -0,0 +1,5 @@
+export 'cropper/image_cropper.dart';
+export 'format_detector/format_detector.dart';
+export 'format_detector/format.dart';
+export 'parser/image_parser.dart';
+export 'parser/image_detail.dart';
