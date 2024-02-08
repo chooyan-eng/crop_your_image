b995ac5323a2969cbca848a95d025fc7aacb4b14
chooyan-eng <chooyan.engineer@gmail.com>
2024-02-08 20:01:08 +0900

    refactor format related code

--- lib/crop_your_image.dart
+++ lib/crop_your_image.dart
@@ -3,11 +3,11 @@ import 'package:crop_your_image/src/logic/format_detector/format_detector.dart';
 import 'package:crop_your_image/src/logic/parser/image_image_parser.dart';
 
 export 'src/widget/widget.dart';
+export 'src/logic/logic.dart';
 
 final defaultImageParser = imageImageParser;
 
 // TODO(chooyan-eng): implement format detector if possible
 const FormatDetector? defaultFormatDetector = null;
 
-const defaultImageCropper =
-    ImageImageCropper(formatDetector: defaultFormatDetector);
+const defaultImageCropper = ImageImageCropper();
--- lib/src/logic/cropper/image_cropper.dart
+++ lib/src/logic/cropper/image_cropper.dart
@@ -3,20 +3,17 @@ import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:crop_your_image/src/logic/format_detector/format.dart';
-import 'package:crop_your_image/src/logic/format_detector/format_detector.dart';
 import 'package:crop_your_image/src/logic/shape.dart';
 
 /// Interface for cropping logic
 abstract class ImageCropper<T> {
-  final FormatDetector? formatDetector;
-
-  const ImageCropper({required this.formatDetector});
+  const ImageCropper();
 
   FutureOr<Uint8List> call({
     required T original,
     required Offset topLeft,
     required Offset bottomRight,
-    ImageFormat format = ImageFormat.jpeg,
-    ImageShape shape = ImageShape.rectangle,
+    ImageFormat outputFormat,
+    ImageShape shape,
   });
 }
