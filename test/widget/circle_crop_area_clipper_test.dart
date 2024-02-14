a0a3f29e7e74d0282f5750409a46c41e37bec041
chooyan-eng <chooyan.engineer@gmail.com>
2024-02-14 11:46:32 +0900

    add golden test to CircleCropAreaClipper

---
+++ test/widget/circle_crop_area_clipper_test.dart
@@ -0,0 +1,35 @@
+import 'package:crop_your_image/src/widget/circle_crop_area_clipper.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('when viewport is 200 x 200', () {
+    final viewportSize = 200.0;
+    testWidgets(
+      'CircleCropAreaClipper clips center with 100x100 size of circle shape'
+      'if passing Rect.fromLTWH(50, 50, 100, 100)',
+      (WidgetTester tester) async {
+        Widget actual = Center(
+          child: SizedBox(
+            width: viewportSize,
+            height: viewportSize,
+            child: RepaintBoundary(
+              child: ClipPath(
+                clipper: CircleCropAreaClipper(Rect.fromLTWH(50, 50, 100, 100)),
+                child: ColoredBox(
+                  color: Color(0xFF0000FF),
+                ),
+              ),
+            ),
+          ),
+        );
+        await tester.pumpWidget(actual);
+        await expectLater(
+          find.byType(RepaintBoundary),
+          matchesGoldenFile(
+              'circle_crop_area_clipper.viewportSizex200.crop_center.png'),
+        );
+      },
+    );
+  });
+}
