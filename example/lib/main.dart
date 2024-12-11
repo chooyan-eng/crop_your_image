f2dcc4b39bc2328863e47f7317ee4aa814010bb9
chooyan-eng <chooyan.engineer@gmail.com>
2024-12-11 23:32:56 +0900

    fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 import 'package:crop_your_image/crop_your_image.dart';
-import 'package:example/my_cropper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
